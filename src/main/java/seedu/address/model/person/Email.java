7aa7efeee0bf3b0eb7af1dec3828a69fe4af7bcd
aditi2313 <52269537+aditi2313@users.noreply.github.com>
2022-02-25 01:04:33 +0800

    Update Email.java

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -45,7 +45,7 @@ public class Email {
     }
 
     /**
-     * Returns if a given string is a valid email.
+     * Returns if a given string is a valid email.\
      */
     public static boolean isValidEmail(String test) {
         return test.matches(VALIDATION_REGEX);
