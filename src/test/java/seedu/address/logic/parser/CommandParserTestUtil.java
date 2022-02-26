8d25c0066a84dec8aa7a96b6953321c75c4fa6df
Halpfrog <62001923+Halpfrog@users.noreply.github.com>
2022-02-26 21:35:18 +0800

    Follow tutorial to add remark command

--- src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -3,7 +3,8 @@ package seedu.address.logic.parser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import seedu.address.logic.commands.Command;
-import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.commands.parser.Parser;
+import seedu.address.logic.commands.parser.exceptions.ParseException;
 
 /**
  * Contains helper methods for testing command parsers.
